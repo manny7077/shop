74f8ba549dfa4d07d74ab7d0ccee84273931438f
manny <marfulmanuel67@gmail.com>
2025-03-26 21:17:22 +0000

    shop name

--- frontend/quasar-project/src/layouts/MainLayout.vue
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-   
+            Grace Hour Cosmetics
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
